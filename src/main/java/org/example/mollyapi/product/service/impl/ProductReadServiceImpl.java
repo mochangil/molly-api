59b89910aefb01b26a80030893020cf68bdced74
Jeonghwan Kim <51739985+hwan20202@users.noreply.github.com>
2025-03-28 23:56:20 +0900

    [#231] test: test 수치 수정

--- src/main/java/org/example/mollyapi/product/service/impl/ProductReadServiceImpl.java
+++ src/main/java/org/example/mollyapi/product/service/impl/ProductReadServiceImpl.java
@@ -53,7 +53,7 @@ public class ProductReadServiceImpl implements ProductReadService {
         Slice<ProductAndThumbnailDto> page = productRepository.findByCondition(condition, pageable, offsetId);
         long endTime = System.currentTimeMillis(); // 종료 시간 기록
         long elapsedTime = endTime - startTime;
-        if (elapsedTime > 1000) {
+        if (elapsedTime > 300) {
             log.info("Execution time: " + (endTime - startTime) + " ms");
         }
         return page.map(this::convertToProductResDto);
