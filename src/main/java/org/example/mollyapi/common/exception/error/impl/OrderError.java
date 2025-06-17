96eb54a0756bc9ee22f9723924ccc568e442d97b
mochangil <ahckddlf1234@gmail.com>
2025-06-17 14:07:49 +0900

    [#247]refactor: 에러 유형 추가

--- src/main/java/org/example/mollyapi/common/exception/error/impl/OrderError.java
+++ src/main/java/org/example/mollyapi/common/exception/error/impl/OrderError.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum OrderError implements CustomError {
 
     INSUFFICIENT_STOCK(HttpStatus.BAD_REQUEST, "재고가 부족하여 결제를 진행할 수 없습니다."),
+    INSUFFICIENT_POINT(HttpStatus.BAD_REQUEST, "사용자 포인트가 부족하여 결제를 진행할 수 없습니다."),
     ORDER_WITHDRAW_REFUND_FAIL(HttpStatus.BAD_REQUEST, "포인트 환불 중 오류가 발생했습니다. 관리자에게 문의하세요."),
     PAYMENT_RETRY_REQUIRED(HttpStatus.PAYMENT_REQUIRED, "결제가 실패했습니다. 다시 시도하시겠습니까? (API: /orders/{orderId}/fail-payment)"),
     INVALID_ORDER(HttpStatus.BAD_REQUEST, "유효하지 않은 결제 요청입니다."),
